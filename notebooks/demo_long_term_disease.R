bde4160524b19e3e026ba7c03eb5e7156b62d647
Antoine Belloir <antoine.belloir@inria.fr>
2024-07-29 18:42:10 +0200

    add option to store output table in Oracle, and to provide connection

--- notebooks/demo_long_term_disease.R
+++ notebooks/demo_long_term_disease.R
@@ -4,9 +4,13 @@ library(dbplyr)
 library(DBI)
 library(glue)
 library(lubridate)
-# Source the following functions to run the code below:
-# - initialize_connection
-# - extract_long_term_disease
+
+is_package <- require(paresnds)
+
+if (!is_package) {
+  source("../R/extract_long_term_disease.R")
+  source("../R/utils.R")
+}
 
 # Retrieve all active long-term diseases in 2010
 # with ICD 10 code corresponding to Parkinson's
@@ -16,9 +20,26 @@ end_date <- as.Date("2010-12-31")
 icd_cod_starts_with <- c("G20")
 
 ald <- extract_long_term_disease(
-    start_date = start_date,
-    end_date = end_date,
-    icd_cod_starts_with = icd_cod_starts_with
+  start_date = start_date,
+  end_date = end_date,
+  icd_cod_starts_with = icd_cod_starts_with
+)
+head(ald)
+
+# You can also provide your own database connection
+# to the extract_drug_dispenses function. This is useful
+# to avoid opening and closing a connection for each
+# extraction function call.
+conn <- initialize_connection()
+start_date <- as.Date("2010-01-01")
+end_date <- as.Date("2010-12-31")
+icd_cod_starts_with <- c("G20")
+
+ald <- extract_long_term_disease(
+  start_date = start_date,
+  end_date = end_date,
+  icd_cod_starts_with = icd_cod_starts_with,
+  conn = conn
 )
 head(ald)
 
@@ -30,8 +51,30 @@ end_date <- as.Date("2010-12-31")
 ald_numbers <- c(8)
 
 ald <- extract_long_term_disease(
-    start_date = start_date,
-    end_date = end_date,
-    ald_numbers = ald_numbers
+  start_date = start_date,
+  end_date = end_date,
+  ald_numbers = ald_numbers
 )
 head(ald)
+
+# If the output_table_name argument is provided,
+# the output will be stored in a table with the
+# given name. This is especially useful when the
+# output table is too large to be stored in memory.
+start_date <- as.Date("2010-01-01")
+end_date <- as.Date("2010-12-31")
+ald_numbers <- c(8)
+output_table_name <- "TMP_LTD"
+print(dbExistsTable(conn, output_table_name))
+
+extract_long_term_disease(
+  start_date = start_date,
+  end_date = end_date,
+  ald_numbers = ald_numbers,
+  output_table_name = output_table_name
+)
+
+print(dbExistsTable(conn, output_table_name))
+query <- glue("SELECT COUNT(*) FROM {output_table_name}")
+result <- dbGetQuery(conn, query)
+print(result)
