8468d75bcc5f0c43ae9d64a9a21c2bc8a09f93a2
Antoine Belloir <antoine.belloir@inria.fr>
2024-08-01 16:30:15 +0200

    add documentation for consultation extraction function

--- R/extract_private_consultations.R
+++ R/extract_private_consultations.R
@@ -1,28 +1,123 @@
-# library(ROracle)
-# library(dplyr)
-# library(dbplyr)
-# library(DBI)
-# library(glue)
-# library(lubridate)
-# source("/sasdata/prd/users/44a001280010899/park/retrieve/utils.R")
-
-
+#' Extraction des consultations en médecine de ville
+#'
+#' Cette fonction permet d'extraire les  consultations en
+#' médecine de ville.
+#' Les consultations dont les dates EXE_SOI_DTD sont comprises
+#' entre start_date et end_date (incluses) sont extraites.
+#' Le décalage de remontée des données est pris en compte
+#' en récupérant également les délivrances dont les dates
+#' FLX_DIS_DTD sont comprises dans les `dis_dtd_lag_months`
+#' mois suivant end_date.
+#' Si pse_spe_codes est fourni, seules les consultations
+#' dont ... sont extraites. Dans le
+#' cas contraire, les consultations pour ...
+#' sont extraites.
+#' Si prs_nat_ref_codes est fourni, seules les consultations
+#' dont ... sont extraites. Dans le
+#' cas contraire, les consultations pour ...
+#' sont extraites.
+#' Si patients_ids est fourni, seules les consultations
+#' des patients dont les identifiants sont dans patients_ids sont
+#' extraites. Dans le cas contraire, les consultations de tous les
+#' patients sont extraites.
+#'
+#' @param start_date Date La date de début de la période
+#'   des consultations à extraire.
+#' @param end_date Date La date de fin de la période
+#'   des consultations à extraire.
+#' @param pse_spe_codes Integer vector Optionnel. Les codes des
+#'   spécialités médicales de médecin à extraire (voir nomenclature IR_SPE_V).
+#' @param prs_nat_ref_codes Integer vector Optionnel. Les codes des
+#'   ...
+#' @param dis_dtd_lag_months Integer Optionnel. Le nombre maximum de
+#'   mois de décalage de FLX_DIS_DTD par rapport à EXE_SOI DTD pris en compte
+#'   pour récupérer les consultations. Par défaut, 7 mois.
+#' @param patients_ids data.frame Optionnel. Un data.frame contenant les
+#'   paires d'identifiants des patients pour lesquels les délivrances de
+#'   médicaments doivent être extraites. Les colonnes de ce data.frame
+#'   doivent être "ben_idt_ano" et "ben_nir_psa" (en minuscules). Les
+#'   "ben_nir_psa" doivent être tous les "ben_nir_psa" associés aux
+#'   "ben_idt_ano" fournis.
+#' @param output_table_name Character Optionnel. Si fourni, les résultats seront
+#'   sauvegardés dans une table portant ce nom dans la base de données au lieu
+#'   d'être retournés sous forme de data frame.
+#' @param conn DBI connection Une connexion à la base de données Oracle.
+#'   Si non fournie, une connexion est établie par défaut.
+#' @return Si output_table_name est NULL, retourne un data.frame contenant les
+#'   consultations. Si output_table_name est fourni, sauvegarde les
+#'   résultats dans la table spécifiée dans Oracle et retourne NULL de manière
+#'   invisible. Dans les deux cas les colonnes de la table de sortie sont :
+#'   - BEN_NIR_PSA : Colonne présente uniquement si les identifiants
+#'   patients (`patients_ids`) ne sont pas fournis. Identifiant SNDS,
+#'   ausi appelé pseudo-NIR.
+#'   - BEN_IDT_ANO : Colonne présente uniquement si les identifiants
+#'   patients (`patients_ids`) sont fournis. Numéro d’inscription
+#'   au répertoire (NIR) anonymisé.
+#'   - EXE_SOI_DTD : Date de la consultation
+#'   - PSE_SPE_COD : Code de spécialité du professionnel de soin exécutant
+#'   (voir nomenclature IR_SPE_V)
+#'   (autres colonnes)
+#'
+#' @examples
+#' \dontrun{
+#' start_date <- as.Date("2010-01-01")
+#' end_date <- as.Date("2010-01-03")
+#' pse_spe_codes <- c()
+#' prs_nat_ref_codes <- c()
+#'
+#' dispenses <- extract_private_consultations(
+#'   start_date = start_date,
+#'   end_date = end_date,
+#'   pse_spe_codes = NULL,
+#'   prs_nat_ref_codes = NULL
+#' )
+#' }
+#' @export
 extract_private_consultations <- function(
     start_date = NULL,
     end_date = NULL,
     pse_spe_codes = NULL,
     prs_nat_ref_codes = NULL,
-    ben_table_name = NULL,
+    dis_dtd_lag_months = 7,
+    patients_ids = NULL,
     output_table_name = NULL,
-    r_output_path = NULL) {
-  conn <- initialize_connection() # Connect to database
+    conn = NULL) {
+  if (is.null(start_date) || is.null(end_date)) {
+    stop("Both start_date and end_date must be provided.")
+  }
 
+  if (!inherits(start_date, "Date") || !inherits(end_date, "Date")) {
+    stop("start_date and end_date must be Date objects.")
+  }
+
+  if (start_date > end_date) {
+    stop("start_date must be earlier than or equal to end_date.")
+  }
+
+  connection_opened <- FALSE
+  if (is.null(conn)) {
+    conn <- initialize_connection()
+    connection_opened <- TRUE
+  }
+
+  if (!is.null(output_table_name)) {
+    if (!is.character(output_table_name)) {
+      stop("output_table_name must be a character string")
+    }
+    table_name <- output_table_name
+    if (DBI::dbExistsTable(conn, table_name)) {
+      warning(paste("Table", table_name, "already exists. It will be overwritten."))
+    }
+  } else {
+    table_name <- paste0("TMP_DISP_", format(Sys.time(), "%Y%m%d_%H%M%S"))
+  }
+  try(
+    DBI::dbRemoveTable(conn, table_name),
+    silent = TRUE
+  )
+  dis_dtd_end_date <- end_date + lubridate::days(30 * dis_dtd_lag_months)
   start_year <- lubridate::year(start_date)
-  end_year <- lubridate::year(end_date) + 1
-  formatted_start_date <- format(start_date, "%Y-%m-%d")
-  formatted_end_date <- format(end_date, "%Y-%m-%d")
-
-  start_time <- Sys.time()
+  end_year <- lubridate::year(dis_dtd_end_date)
 
   first_non_archived_year <- get_first_non_archived_year(conn)
 
